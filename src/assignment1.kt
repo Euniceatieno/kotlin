ce23f1c7f6e02f759140955753235da826d1b311
Euniceatieno <eunniceatieno@gmail.com>
2021-02-20 23:36:14 +0300

    first assignment

---
+++ src/assignment1.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var field="NAME-Eunice Atieno"
+    var fieldTwo="AGE-20"
+    var fieldThree="PHONENUMBER-0798676088"
+    var fieldFour="WEIGHT-55"
+    var fieldSix="KENYAN-"
+    var fieldFive:Boolean = true
+    println(field)
+    println(fieldTwo)
+    println(fieldThree)
+    println(fieldFour)
+    print(fieldSix)
+    print("$fieldFive")
+
+
+
+
+
+}
